1f9a542592ae0e2b75e3f0157a5b9639296564c7
Jacob Hofer <jacobhofer1@gmail.com>
2020-03-14 18:45:41 -0600

    Added notifications
    Silent notifications will now appear anytime the music is paused, or an ad begins/ends

--- Spotify Muter/MainWindow.xaml.cs
+++ Spotify Muter/MainWindow.xaml.cs
@@ -16,162 +16,195 @@ using System.Diagnostics;
 using System.Threading;
 using System.Runtime.InteropServices;
 using VideoPlayerController;
+using System.IO;
+
+using Windows.UI.Notifications;
+using Windows.Data.Xml.Dom;
 
 
 
 namespace Spotify_Muter {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    /// 
-    public partial class MainWindow : Window {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	/// 
+	public partial class MainWindow : Window {
 
-        private Label songLbl, artistLbl;
-        private List<string> artistBlacklist;
+		private Label songLbl, artistLbl;
+		private List<string> artistBlacklist;
 
-        public MainWindow() {
+		public MainWindow() {
 
-            InitializeComponent();
-            Title = "Spotify Muter";
-            ResizeMode = ResizeMode.CanMinimize;
+			InitializeComponent();
+			Title = "Spotify Muter";
+			ResizeMode = ResizeMode.CanMinimize;
 
-            // Get the label
-            songLbl = (Label)this.FindName("Song");
-            artistLbl = (Label)this.FindName("Artist");
-            try {
-                artistBlacklist = System.IO.File.ReadAllLines("blacklist.txt").ToList();
-            } catch (Exception e) {
-                // File is corrupt, does not exist, etc.
-                System.IO.File.Create("blacklist.txt");
-                artistBlacklist = new List<string>();
-            }
+			// Get the label
+			songLbl = (Label)this.FindName("Song");
+			artistLbl = (Label)this.FindName("Artist");
+			try {
+				artistBlacklist = System.IO.File.ReadAllLines("blacklist.txt").ToList();
+			} catch (Exception e) {
+				// File is corrupt, does not exist, etc.
+				System.IO.File.Create("blacklist.txt");
+				artistBlacklist = new List<string>();
+			}
 
-            CompositionTarget.Rendering += loop;
-        }
+			CompositionTarget.Rendering += loop;
 
-        private string prevArtist = "";
-        private string currentArtist = "";
-        private string prevTrackName = "";
+		}
 
-        // Gets called every frame
-        private void loop(object sender, EventArgs e) {
+		private string prevArtist = "";
+		private string currentArtist = "";
+		private string prevTrackName = "";
 
-            // Obtain information about the track
-            string trackInfo = GetSpotifyTrackInfo();
+		// Gets called every frame
+		private void loop(object sender, EventArgs e) {
 
-            if (trackInfo == "Spotify is not running!" || trackInfo == "Paused") {
-                return;
-            }
+			// Obtain information about the track
+			string trackInfo = GetSpotifyTrackInfo();
 
-            // Extract the artist and track name
-            string artist = "";
-            string trackName = "";
-            for (int i = 0; i < trackInfo.Length; i++) {
-                char cur = trackInfo[i];
-                if (cur == '-') {
-                    if (i != 0 && i != trackInfo.Length - 2) {
-                        char prev = trackInfo[i - 1];
-                        char next = trackInfo[i + 1];
-                        if (prev == ' ' && next == ' ') {
-                            artist = trackInfo.Substring(0, i - 1);
-                            trackName = trackInfo.Substring(i + 2);
-                            break;
-                        }
-                    }
-                }
-            }
+			if (trackInfo == "Spotify is not running!" || trackInfo == "Paused") {
+				return;
+			}
 
-
-            // If no data was extracted, that means its an ad, or paused
-            if (artist == "" || artistBlacklist.Contains(artist)) {
-                if (trackInfo == "Spotify Free") {
-                    artist = "";
-                    trackName = "Paused";
-                } else {
-                    artist = "";
-                    trackName = "Advertisement";
-                }
-
-            }
-
-            // Update labels
-            songLbl.Content = trackName;
-            artistLbl.Content = artist;
-
-            currentArtist = artist;
-
-            if (artist != prevArtist || trackName != prevTrackName) {
-                infoChange(artist, prevArtist, trackName, prevTrackName);
-            }
-
-            prevArtist = artist;
-            prevTrackName = trackName;
-
-        }
-
-        // Run whenever the artist listening to changes
-        private void infoChange(string newArtist, string prevArtist, string newTrack, string prevTrack) {
-
-            if (newTrack.Equals("Spotify") || newTrack.Equals("Advertisement") || artistBlacklist.Contains(newArtist)) {
-                // If the artist is Spotify or Advertisement
-                var proc = Process.GetProcessesByName("Spotify").FirstOrDefault(p => !string.IsNullOrWhiteSpace(p.MainWindowTitle));
-                if (proc != null) {
-                    // Mute spotify
-                    AudioManager.SetApplicationMute(proc.Id, true);
-
-                }
-
-            } else {
-                // Not an ad
-                var proc = Process.GetProcessesByName("Spotify").FirstOrDefault(p => !string.IsNullOrWhiteSpace(p.MainWindowTitle));
-                if (proc != null) {
-                    // Unmute Spotify
-                    Task.Delay(500).ContinueWith(_ => {
-                        AudioManager.SetApplicationMute(proc.Id, false);
-                    });
-                }
-            }
-        }
+			// Extract the artist and track name
+			string artist = "";
+			string trackName = "";
+			for (int i = 0; i < trackInfo.Length; i++) {
+				char cur = trackInfo[i];
+				if (cur == '-') {
+					if (i != 0 && i != trackInfo.Length - 2) {
+						char prev = trackInfo[i - 1];
+						char next = trackInfo[i + 1];
+						if (prev == ' ' && next == ' ') {
+							artist = trackInfo.Substring(0, i - 1);
+							trackName = trackInfo.Substring(i + 2);
+							break;
+						}
+					}
+				}
+			}
 
 
-        private string GetSpotifyTrackInfo() {
+			// If no data was extracted, that means its an ad, or paused
+			if (artist == "" || artistBlacklist.Contains(artist)) {
+				if (trackInfo == "Spotify Free") {
+					artist = "";
+					trackName = "Paused";
+				} else {
+					artist = "";
+					trackName = "Advertisement";
+				}
 
-            var proc = Process.GetProcessesByName("Spotify").FirstOrDefault(p => !string.IsNullOrWhiteSpace(p.MainWindowTitle));
-            // Check that spotify is open
-            if (proc == null) {
-                return "Spotify is not running!";
-            }
+			}
 
-            // The main title contains info about the current track, or ad info
-            return proc.MainWindowTitle;
-        }
+			// Update labels
+			songLbl.Content = trackName;
+			artistLbl.Content = artist;
 
-        private void RegisterAd(object sender, RoutedEventArgs e) {
-            if (!artistBlacklist.Contains(currentArtist)) {
-                artistBlacklist.Add(currentArtist);
-                infoChange(currentArtist, prevArtist, "", "");
-            }
-        }
+			currentArtist = artist;
 
-        public void WindowClosing(object sender, EventArgs e) {
-            // Unmute spotify when we close
-            var proc = Process.GetProcessesByName("Spotify").FirstOrDefault(p => !string.IsNullOrWhiteSpace(p.MainWindowTitle));
-            if (proc != null) {
-                AudioManager.SetApplicationMute(proc.Id, false);
-            }
+			if (artist != prevArtist || trackName != prevTrackName) {
+				infoChange(artist, prevArtist, trackName, prevTrackName);
+			}
 
-            try {
-                System.IO.File.WriteAllLines("blacklist.txt", artistBlacklist.ToArray());
-            } catch (Exception ex) {
-                System.IO.File.Create("blacklist.txt");
-            } finally {
-                System.IO.File.WriteAllLines("blacklist.txt", artistBlacklist.ToArray());
-            }
+			prevArtist = artist;
+			prevTrackName = trackName;
+
+		}
+
+		// Run whenever the artist listening to changes
+		private void infoChange(string newArtist, string prevArtist, string newTrack, string prevTrack) {
+
+			if (newTrack.Equals("Spotify") || newTrack.Equals("Advertisement") || artistBlacklist.Contains(newArtist)) {
+				// If the artist is Spotify or Advertisement
+				var proc = Process.GetProcessesByName("Spotify").FirstOrDefault(p => !string.IsNullOrWhiteSpace(p.MainWindowTitle));
+				if (proc != null) {
+					// Mute spotify
+					AudioManager.SetApplicationMute(proc.Id, true);
+
+					// Send notification
+					sendNotification("SpotifyMuter", "An ad has started, so your audio was muted.", "");
+				}
+
+			} else {
+				// Not an ad
+				var proc = Process.GetProcessesByName("Spotify").FirstOrDefault(p => !string.IsNullOrWhiteSpace(p.MainWindowTitle));
+				if (proc != null) {
+					// Unmute Spotify
+					Task.Delay(500).ContinueWith(_ => {
+						AudioManager.SetApplicationMute(proc.Id, false);
+					});
+				}
+
+				// Send notifications
+				if (newTrack.Equals("Paused")) {
+					sendNotification("SpotifyMuter", "Your music was paused.", "");
+				} else if (prevTrack.Equals("Spotify") || prevTrack.Equals("Advertisement") || artistBlacklist.Contains(prevArtist)) {
+					sendNotification("SpotifyMuter", "The ads have ended, enjoy your music!", "");
+				}
+
+			}
+		}
+
+
+		private string GetSpotifyTrackInfo() {
+
+			var proc = Process.GetProcessesByName("Spotify").FirstOrDefault(p => !string.IsNullOrWhiteSpace(p.MainWindowTitle));
+			// Check that spotify is open
+			if (proc == null) {
+				return "Spotify is not running!";
+			}
+
+			// The main title contains info about the current track, or ad info
+			return proc.MainWindowTitle;
+		}
+
+		private void RegisterAd(object sender, RoutedEventArgs e) {
+			if (!artistBlacklist.Contains(currentArtist)) {
+				artistBlacklist.Add(currentArtist);
+				infoChange(currentArtist, prevArtist, "", "");
+			}
+		}
+
+		private void sendNotification(string title, string line1, string line2) {
+
+			XmlDocument toastXml = new XmlDocument();
+			toastXml.LoadXml("<toast duration=\"short\"><visual><binding template=\"ToastText04\"><text id=\"1\">" + title + "</text><text id=\"2\">" + line1 + "</text><text id=\"3\">" + line2 + "</text></binding></visual><audio silent=\"true\"/></toast>");
+
+			XmlElement audio = toastXml.CreateElement("audio");
+			audio.SetAttribute("silent", "true");
+
+			Console.WriteLine(toastXml.GetXml());
+
+			ToastNotification toast = new ToastNotification(toastXml);
+
+			ToastNotificationManager.CreateToastNotifier("SpotifyMuter").Show(toast);
 
 
 
-        }
+		}
+
+		public void WindowClosing(object sender, EventArgs e) {
+			// Unmute spotify when we close
+			var proc = Process.GetProcessesByName("Spotify").FirstOrDefault(p => !string.IsNullOrWhiteSpace(p.MainWindowTitle));
+			if (proc != null) {
+				AudioManager.SetApplicationMute(proc.Id, false);
+			}
+
+			try {
+				System.IO.File.WriteAllLines("blacklist.txt", artistBlacklist.ToArray());
+			} catch (Exception ex) {
+				System.IO.File.Create("blacklist.txt");
+			} finally {
+				System.IO.File.WriteAllLines("blacklist.txt", artistBlacklist.ToArray());
+			}
 
 
-    }
+
+		}
+
+
+	}
 }
